0ec542363395230da46b6b3262dab8bf8b70afd2
DamianBurboa <damianburboa1997@gmail.com>
2019-09-05 19:51:00 -0700

    Add project files.

---
+++ Consultorio2/Paciente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consultorio2
+{
+    class Paciente
+    {
+
+        //Atributos (variables)
+        string nombre;
+        string direccion;
+        string telefono;
+        DateTime fechaDeNacimiento;
+        string enfermedadesCronicas;
+
+        //Métodos (funciones)
+    }
+}
